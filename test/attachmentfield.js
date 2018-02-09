6d64a2cc7a349a31f6b4d342549e3923d10a12e9
Patrick Leeb <patrick.leeb@gmail.com>
2018-02-09 11:42:02 +0100

    Fixed AttachmentField, added Test Case

---
+++ test/attachmentfield.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var chai = require('chai');
+var Promise = require('bluebird');
+var should = chai.should();
+var expect = chai.expect;
+var Client = require('../lib/Client');
+var File = require('../lib/File');
+var Signer = require('../lib/Signer');
+var Document = require('../lib/Document');
+var AttachmentField = require('../lib/AttachmentField');
+var path = require('path');
+var config = require('../examples/config');
+
+var key = config.accessKey;
+var businessId = config.businessId;
+
+
+describe("AttachmentField", function () {
+
+  it('should get added to a Document without a problem', function(done){
+    
+    var document = new Document();
+    document.setDocumentHash("My Document");
+    document.setTitle("Title goes here");
+    document.setRequesterEmail("tester@gmail.com");
+    document.setIsDraft(false);
+
+    var file = new File({
+      name: 'My File',
+      filePath: path.join(__dirname, 'raw.pdf'),
+    });
+    document.appendFile(file);
+
+    var attachmentField = new AttachmentField();
+    attachmentField.setX(30);
+    attachmentField.setY(30);
+    attachmentField.setWidth(200);
+    attachmentField.setHeight(50);
+    attachmentField.setRequired(true);
+    attachmentField.setPage("1");
+    attachmentField.setSigner("1");
+    document.appendFormField(attachmentField);
+
+    expect(document.toObject()).to.have.property('fields').and.have.length(1); // Success
+    done();
+  });
+
+
+  it('should get uploaded without an error', function(done){
+    this.timeout(10000); 
+
+    var document = new Document();
+    document.setDocumentHash("My Document");
+    document.setTitle("AttachmentField Test");
+    document.setRequesterEmail("tester@gmail.com");
+    document.setIsDraft(true);
+
+    var signer = new Signer();
+    signer.setName('Tester Test');
+    signer.setEmail('tester@gmail.com')
+    document.appendSigner(signer);
+
+    var file = new File({
+      name: 'My File',
+      filePath: path.join(__dirname, 'raw.pdf'),
+    });
+    document.appendFile(file);
+
+    var attachmentField = new AttachmentField();
+    attachmentField.setX(30);
+    attachmentField.setY(30);
+    attachmentField.setWidth(200);
+    attachmentField.setHeight(50);
+    attachmentField.setRequired(true);
+    attachmentField.setPage("1");
+    attachmentField.setSigner("1");
+    document.appendFormField(attachmentField);
+
+    var client = new Client(key, businessId);
+
+    client.createDocument(document).then(function(doc) {
+      expect( doc.toObject() ).to.be.an.instanceof(Document);
+      done();
+    })
+    .catch(function(err) {
+      done(error)
+    });
+  
+  });
+
+  
+});
+
