9f20fc796cfb7811a8fcb0d4af1d2bdc2b3bb9aa
Milan Latinovic <milan.latinovic@stack.holdings>
2021-10-13 11:38:50 +0200

    var to let

--- examples/get_all_documents.js
+++ examples/get_all_documents.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var Client = require('../index').Client;
-var Document = require('../index').Document;
+let Client = require('../index').Client;
+let Document = require('../index').Document;
 
-var config = require('./config');
+const config = require('./config');
 
-var client = new Client(config.accessKey, config.businessId);
+let client = new Client(config.accessKey, config.businessId);
 
 client.getAllDocuments().then(function (documents) {
     console.log(documents.length, ' documents found');
     documents.forEach(document => {
-        var files = document.getFiles();
+        let files = document.getFiles();
 
         files.forEach(file => {
             // do something with files
